796bfea820346f90bb7a75360d4ec899143669c4
Mohamed Eid <mohamed868hu@gmail.com>
2026-08-05 19:59:28 +0300

    Change BrowserRouter to HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './auth/AuthContext'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Help-Desk-Lite">
+    <HashRouter>
       <AuthProvider><App /></AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
